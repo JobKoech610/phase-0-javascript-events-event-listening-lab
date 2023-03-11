cee07f3e06680be29a7e1e5955836dcbd2ef31f2
Job koech <jobkoech610@gmail.com>
2023-03-11 14:07:42 +0300

    Completed assingnment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+  //select by id button
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
 }
+
+// call the function
+addingEventListener();
+
+
+
+  
